a06b867d5794c01f5d81957dc61270f23b3f361f
Dimitry Ivanov <2@ivanoff.org.ua>
2023-09-09 22:46:52 +0300

    add types

---
+++ README.md
@@ -0,0 +1,86 @@
+# Ecologism Icons for React Native
+
+### react-native-ico-ecologism
+
+49 Vector Icons for React Native
+
+<img src="./static/battery-with-recycle-symbol.png" alt="battery-with-recycle-symbol" width="150" height="150"> <img src="./static/corn.png" alt="corn" width="150" height="150"> <img src="./static/danger-signal.png" alt="danger-signal" width="150" height="150">
+
+## List of icons
+
+- [List of Ecologism Icons](http://ico.simpleness.org/pack/ecologism)
+
+## Usage
+
+```
+import Icon from 'react-native-ico-ecologism';
+
+
+// Inside some view component
+render() {
+    return (
+        <>
+          <Icon name="battery-with-recycle-symbol" />
+          <Icon name="corn" height="40" width="40" />
+          <Icon name="danger-signal" color="red" />
+          <Icon name="corn" badge="10" />
+          <Icon name="corn" badge={{value: 'A', fontSize: 25, radius: 22, position:'top_left', color:'orange', backgroundColor:'blue'}}/>
+          <Icon name="battery-with-recycle-symbol" background="circle" />
+          <Icon name="battery-with-recycle-symbol" background={{ type: "button", color: 'green' }} />
+        </>
+    );
+}
+
+```
+
+## Installation
+
+#### yarn
+
+```bash
+yarn add react-native-ico-ecologism react-native-svg
+```
+
+#### npm
+
+```bash
+npm install --save react-native-ico-ecologism react-native-svg
+```
+
+### Link react-native-svg
+
+```bash
+react-native link react-native-svg
+```
+
+### pod install ( for iOS )
+
+```
+cd ios && pod install && cd ..
+```
+
+## API
+
+### <Icon name [color width height background badge ...rest] />
+
+Returns a SvgXml icon by name and group.
+
+ name | optional | default value | description | examples
+------|----------|---------------|-------------|---------
+name | no |  | name of icon | "battery-with-recycle-symbol"
+color | yes | | line color, css style | "#00ff00", "#0f0", "green"
+width | yes | 20 | width of the icon | 40
+height | yes | 20 | height of the icon | 40
+background | no | | background type | "circle"
+background | no | | background object | {type: "circle", color: 'yellow'}
+badge | no | | badge string | "10"
+badge | no | | badge object | {value: 'A', fontSize: 25, radius: 22, position:'top_left', color:'orange', backgroundColor:'blue'}
+...rest | no | | other props | style={{backgroundColor: "#00f"}}
+
+## Icons Made by
+
+[Dave Gandy](https://www.flaticon.com/authors/dave-gandy)
+
+## Created by
+
+Dimitry Ivanov <2@ivanoff.org.ua> # curl -A cv ivanoff.org.ua
---
+++ typings.d.ts
@@ -0,0 +1,89 @@
+declare module 'react-native-ico-ecologism' {
+    import { ReactNode } from 'react';
+
+    type iconNames = 'battery-status-symbol' |
+      'battery-tool-with-bolt-sign' |
+      'battery-with-a-bolt-symbol' |
+      'battery-with-recycle-symbol' |
+      'corn' |
+      'danger-signal' |
+      'descending-temperature-on-thermometer-tool' |
+      'earth-between-hands' |
+      'ecological-fuel-bomb' |
+      'ecological-generator-tool-of-rotatory-fan' |
+      'ecological-solar-power-for-a-battery' |
+      'ecological-tool-for-winds-to-generate-energy' |
+      'electric-car-ecological-transport' |
+      'electrical-lines-tower-support' |
+      'factory-contamination-towers' |
+      'factory-tower-with-biohazard-smoke' |
+      'factory-towers-spreading-smoke' |
+      'faucet-side-view-with-falling-drop-of-water' |
+      'flower-shape-of-five-petals' |
+      'fuel-tank' |
+      'fully-charged-battery' |
+      'hand-with-a-leaf' |
+      'house-side-view' |
+      'human-feet-shape' |
+      'leaf-shape' |
+      'leaves' |
+      'lightbulb-black-shape' |
+      'lightbulb-with-a-bolt' |
+      'low-battery-status' |
+      'o2-symbol-in-a-cloud' |
+      'person-throwing-paper-to-a-trash-can' |
+      'planet-grid-with-a-leaf-international-ecological-symbol' |
+      'planetary-grid' |
+      'raindrops-falling-of-a-black-cloud' |
+      'recycle-symbol-of-three-leaves' |
+      'rural-house-with-wind-mill' |
+      'star-shape-with-circular-arrows-circle' |
+      'sun-ecological-power' |
+      'thermometer-measuring-ascending-temperature' |
+      'thermometer-temperature-control-tool' |
+      'thermometer-tool' |
+      'thermometer' |
+      'toxic-tank-container-with-ecological-risk' |
+      'tree-silhouette' |
+      'watering-can-tool-silhouette-of-water-container-for-garden' |
+      'watering-tool-for-gardening' |
+      'whale-oceanic-mammal-side-view' |
+      'wind-mill-ecological-generator' |
+      'world';
+
+    type backgroundType = 'circle' | 'rect' | 'button';
+
+    type positionType = 'top' | 'bottom' | 'right' | 'left' |
+      'top_right' | 'top_left' | 'bottom_right' | 'bottom_left';
+
+    type backgroundTypeObj = {
+      type?: backgroundType;
+      color?: string;
+      radius?: number;
+      borderRadius?: number;
+    };
+
+    type badgeTypeObj = {
+      value?: number | string;
+      fontSize?: string;
+      position?: positionType;
+      color?: string;
+      backgroundColor?: string;
+      radius?: number;
+    };
+
+    interface IconProps {
+      name: iconNames;
+      width?: number;
+      height?: number;
+      color?: string;
+      stroke?: number;
+      background?: backgroundType | backgroundTypeObj;
+      badge?: number | string | badgeTypeObj;
+    }
+
+    const Icon: (props: IconProps) => ReactNode;
+
+    export { iconNames };
+    export default Icon;
+}
